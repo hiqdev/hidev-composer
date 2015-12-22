9b7ce29d857b499f1c8c7975783b647f88c7087b
Andrii Vasyliev <sol@hiqdev.com>
2015-12-22 14:20:27 +0000

    minor

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 
 /*
- * Scrutinizer plugin for HiDev
+ * Composer plugin for HiDev
  *
- * @link      https://github.com/hiqdev/hidev-scrutinizer
- * @package   hidev-scrutinizer
+ * @link      https://github.com/hiqdev/hidev-composer
+ * @package   hidev-composer
  * @license   BSD-3-Clause
  * @copyright Copyright (c) 2015, HiQDev (http://hiqdev.com/)
  */

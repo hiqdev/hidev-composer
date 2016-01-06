7812dfc995eac2c5b138b6e8f04e60e0483dd1f5
Andrii Vasyliev <sol@hiqdev.com>
2016-01-06 12:39:19 +0000

    redoing with yii2-extraconfig

---
+++ src/yii2-extraconfig.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * Composer plugin for HiDev
+ *
+ * @link      https://github.com/hiqdev/hidev-composer
+ * @package   hidev-composer
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015, HiQDev (http://hiqdev.com/)
+ */
+
+return [
+    'components' => [
+        'goals' => [
+            'composer'       => 'hidev\composer\goals\ComposerGoal',
+            'composer.json'  => 'hidev\composer\goals\ComposerJsonGoal',
+            'packagist'      => 'hidev\composer\goals\PackagistGoal',
+            'readme'         => [
+                'markdownBadges' => [
+                    'packagist.stable'    => '[![Latest Stable Version](https://poser.pugx.org/{{ config.composer.fullName }}/v/stable)](https://packagist.org/packages/{{ config.composer.fullName }})',
+                    'packagist.unstable'  => '[![Latest Unstable Version](https://poser.pugx.org/{{ config.composer.fullName }}/v/unstable)](https://packagist.org/packages/{{ config.composer.fullName }})',
+                    'packagist.license'   => '[![License](https://poser.pugx.org/{{ config.composer.fullName }}/v/license)](https://packagist.org/packages/{{ config.composer.fullName }})',
+                    'packagist.downloads' => '[![Total Downloads](https://poser.pugx.org/{{ config.composer.fullName }}/downloads)](https://packagist.org/packages/{{ config.composer.fullName }})',
+                ],
+            ],
+        ],
+    ],
+];

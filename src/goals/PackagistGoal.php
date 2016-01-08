bdd9ea89fed90e19b50dbeb4c7763e2ca31428e0
Andrii Vasyliev <sol@hiqdev.com>
2016-01-08 16:26:35 +0000

    + empty PackagistGoal

---
+++ src/goals/PackagistGoal.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Composer plugin for HiDev
+ *
+ * @link      https://github.com/hiqdev/hidev-composer
+ * @package   hidev-composer
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hidev\composer\goals;
+
+/**
+ * Goal for Packagist.
+ */
+class PackagistGoal extends \hidev\goals\DefaultGoal
+{
+}
